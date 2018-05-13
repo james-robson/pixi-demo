58ff0809468fec538bc45bda1e217aead1c8c599
james.robson@tombola.com <james.robson@tombola.com>
2018-05-13 22:44:19 +0100

    Added a circle to the canvas to complete the basic tutorial

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
 import * as PIXI from 'pixi.js';
 
-function createPixiCanvas(): HTMLCanvasElement {
-  const canvas = new PIXI.Application({ width: 640, height: 360 });
-  return canvas.view;
+function createPixiApp(): PIXI.Application {
+    return new PIXI.Application({ width: 640, height: 360 });
 }
 
-document.body.appendChild(createPixiCanvas());
+function addGraphicsToApp(appToUpdate: PIXI.Application): void {
+    const circle = new PIXI.Graphics();
+    circle.beginFill(0x5cafe2);
+    circle.drawCircle(0, 0, 80);
+    circle.x = 320;
+    circle.y = 180;
+    appToUpdate.stage.addChild(circle);
+}
+
+const app = createPixiApp();
+const view = app.view;
+
+document.body.appendChild(view);
+
+addGraphicsToApp(app);
