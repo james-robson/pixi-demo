8689b40f9e8fb87e23ba047a5f350d86129a355e
james.robson@tombola.com <james.robson@tombola.com>
2018-05-19 12:46:28 +0100

    Make the game window full screen and relatively position the elements

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,40 @@
 import * as PIXI from 'pixi.js';
+import { Application } from 'pixi.js';
+import './main.css';
+
+let app: Application;
+
+window.addEventListener('resize', () => {
+    app.renderer.resize(window.innerWidth, window.innerHeight);
+    // Remove all the old child elements
+    while (app.stage.children[0]) {
+        app.stage.removeChild(app.stage.children[0]);
+    }
+    addGraphicsToApp(app);
+});
+
+window.addEventListener('load', () => {
+    bootstrap();
+    }, false);
 
 function createPixiApp(): PIXI.Application {
-    return new PIXI.Application({ width: 640, height: 360 });
+    return new PIXI.Application({ width: window.innerWidth, height: window.innerHeight });
 }
 
 function addGraphicsToApp(appToUpdate: PIXI.Application): void {
     const circle = new PIXI.Graphics();
     circle.beginFill(0x5cafe2);
     circle.drawCircle(0, 0, 80);
-    circle.x = 320;
-    circle.y = 180;
+    circle.x = window.innerWidth / 2;
+    circle.y = window.innerHeight / 2;
     appToUpdate.stage.addChild(circle);
 }
 
-const app = createPixiApp();
-const view = app.view;
+function bootstrap(): void {
+    app = createPixiApp();
+    const view = app.view;
 
-document.body.appendChild(view);
+    document.body.appendChild(view);
 
-addGraphicsToApp(app);
+    addGraphicsToApp(app);
+}
