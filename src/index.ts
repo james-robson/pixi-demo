7cdf4ff0304bd19a7df018a1641bb777db87458d
james.robson@tombola.com <james.robson@tombola.com>
2018-07-08 10:53:37 +0100

    Menu system with 1/2 player

--- src/index.ts
+++ src/index.ts
@@ -59,7 +59,7 @@ window.addEventListener('load', () => {
         google: {
             families: ['Press Start 2P']
         },
-        active: startPlaying
+        active: menu
     });
 }, false);
 
@@ -106,13 +106,43 @@ function win (): void {
     app.stage.addChild(winText);
 }
 
-function startPlaying(): void {
+function startPlaying(settings: IGameSettings): void {
     state = play;
     addGraphicsToApp(app);
     const element = document.getElementById('loadingContainer');
     element.classList.add('hidden');
 
     app.ticker.add((delta) => gameLoop(delta));
+}
+
+function menu(): void {
+    const element = document.getElementById('loadingContainer');
+    element.classList.add('hidden');
+
+    const menuTitle = new PIXI.Text(`PONG`, {fontFamily : 'Press Start 2P', fontSize: 52, fill : 0xffffff, align : 'center'});
+    const onePlayerText = new PIXI.Text(`1 PLAYER`, {fontFamily : 'Press Start 2P', fontSize: 52, fill : 0xffffff, align : 'center'});
+    const twoPlayerText = new PIXI.Text(`2 PLAYER`, {fontFamily : 'Press Start 2P', fontSize: 52, fill : 0xffffff, align : 'center'});
+    const menuContainer = new PIXI.Container();
+    menuContainer.height = window.innerHeight;
+    menuContainer.width = window.innerWidth;
+    onePlayerText.interactive = true;
+    onePlayerText.buttonMode = true;
+    twoPlayerText.interactive = true;
+    twoPlayerText.buttonMode = true;
+    twoPlayerText.on('click', (event: Event) => {
+        app.stage.removeChild(menuContainer);
+        startPlaying({mode: 'twoPlayer'});
+     });
+    menuTitle.anchor.set(0.5, 0.5);
+    onePlayerText.anchor.set(0.5, 0.5);
+    twoPlayerText.anchor.set(0.5, 0.5);
+    menuTitle.position.set(window.innerWidth / 2, window.innerHeight / 4);
+    onePlayerText.position.set(window.innerWidth / 2, window.innerHeight / 2);
+    twoPlayerText.position.set(window.innerWidth / 2, window.innerHeight / 1.5);
+    menuContainer.addChild(menuTitle);
+    menuContainer.addChild(onePlayerText);
+    menuContainer.addChild(twoPlayerText);
+    app.stage.addChild(menuContainer);
 }
 
 function play (delta: number): void {
@@ -237,4 +267,8 @@ function detectMovement(): void {
     if (playerTwoKeyboardDown.isDown && (rightPaddle.sprite.y + rightPaddle.sprite.height) < window.innerHeight) {
         rightPaddle.moveDown();
     }
+}
+
+interface IGameSettings {
+    mode: 'onePlayer' | 'twoPlayer';
 }
